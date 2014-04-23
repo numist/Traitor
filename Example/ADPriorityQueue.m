92eb009a144442d8a1a6f4e4073e2d8094f743e6
Scott Perry <github@numist.net>
2014-04-22 19:57:52 -0700

    Priority queue implementation using NNArray trait

---
+++ Example/ADPriorityQueue.m
@@ -0,0 +1,63 @@
+//  Created by Scott Perry on 04/22/14.
+//  Copyright © 2014 Scott Perry.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#import "ADPriorityQueue.h"
+
+
+@interface ADPriorityQueue ()
+
+@property (nonatomic, strong) NSMutableArray *backingStore;
+
+@end
+
+
+@implementation ADPriorityQueue
+
+- (instancetype)init;
+{
+    if (!(self = [super init])) { return nil; }
+    
+    self->_backingStore = [NSMutableArray new];
+    
+    return self;
+}
+
+#pragma mark NNArrayTrait
+
+- (NSUInteger)count;
+{
+    return self.backingStore.count;
+}
+
+- (id)objectAtIndex:(NSUInteger)index;
+{
+    return [self.backingStore objectAtIndex:index];
+}
+
+#pragma mark ADPriorityQueue
+
+- (void)insertObject:(id<ADPriorityObject>)object;
+{
+    NSUInteger insertionIndex = [self.backingStore indexOfObject:object
+                                                   inSortedRange:NSMakeRange(0, self.count)
+                                                         options:NSBinarySearchingInsertionIndex
+                                                 usingComparator:^(id<ADPriorityObject> obj1, id<ADPriorityObject> obj2) {
+
+        return (NSComparisonResult)[@(obj1.priority) compare:@(obj2.priority)];
+    }];
+    [self.backingStore insertObject:object atIndex:insertionIndex];
+}
+
+- (void)removeObjectAtIndex:(NSUInteger)index;
+{
+    [self.backingStore removeLastObject];
+}
+
+@end
